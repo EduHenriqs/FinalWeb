b9e79e78088d5c97ff81f28ea8dc91dbfc2d7a72
luizmenos <110849879+luizmenos@users.noreply.github.com>
2024-06-22 21:23:07 -0300

    Add files via upload

---
+++ includes/rodape.php
@@ -0,0 +1,5 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/datatables.min.js"></script>
+<script src="js/pt-BR.js"></script>
+<script src="js/sweetalert2.all.min.js"></script>

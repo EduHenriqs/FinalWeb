d954a76809062e716232d51c0c6af125d8ce2e93
EduHenriqs <110845651+EduHenriqs@users.noreply.github.com>
2024-06-22 21:28:25 -0300

    Add files via upload

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -3,9 +3,15 @@
 <link rel="stylesheet" href="css/datatables.min.css">
 <link rel="stylesheet" href="css/fontawesome-all.min.css">
 
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom p-2">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4">Cadastrinho Cadastroso</span>
+      </a>
 
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/datatables.min.js"></script>
-<script src="js/jquery.min.js"></script>
-<script src="js/pt-BR.js"></script>
-<script src="js/sweetalert2.all.min.js"></script>
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Início</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Produtos</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Categorias</a></li>
+      </ul>
+    </header>

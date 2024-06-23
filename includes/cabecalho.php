ba09dd46b2c264f324329bd6fbbfe88ae7c13b11
EduHenriqs <110845651+EduHenriqs@users.noreply.github.com>
2024-06-22 21:20:23 -0300

    Add files via upload

---
+++ includes/cabecalho.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/datatables.css">
+<link rel="stylesheet" href="css/datatables.min.css">
+<link rel="stylesheet" href="css/fontawesome-all.min.css">
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/datatables.min.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/pt-BR.js"></script>
+<script src="js/sweetalert2.all.min.js"></script>

b771acd9ed09b7c78c5404acc55b3037cc095a8a
luizmenos <110849879+luizmenos@users.noreply.github.com>
2024-06-24 10:44:57 -0300

    Add files via upload

--- includes/database.php
+++ includes/database.php
@@ -1,45 +1,41 @@
-
-
 <?php
+error_log('Including database.php');
 
-class Database
-{
+if (class_exists('Database')) {
+    error_log('Class Database already exists');
+    return;
+}
+error_log('Defining class Database');
+
+class Database {
     private $mysqli;
 
-    public function __construct($host, $username, $password, $database)
-    {
+    public function __construct($host, $username, $password, $database) {
         $this->mysqli = new mysqli($host, $username, $password, $database);
 
         if ($this->mysqli->connect_error) {
-            die('Erro de conexão (' . $this->mysqli->connect_errno . ') '
-                . $this->mysqli->connect_error);
+            die('Erro de conexão (' . $this->mysqli->connect_errno . ') ' . $this->mysqli->connect_error);
         }
     }
 
-    public function query($sql)
-    {
+    public function query($sql) {
         return $this->mysqli->query($sql);
     }
 
-    public function prepare($sql)
-    {
+    public function prepare($sql) {
         return $this->mysqli->prepare($sql);
     }
 
-    public function escapeString($string)
-    {
+    public function escapeString($string) {
         return $this->mysqli->real_escape_string($string);
     }
 
-    public function close()
-    {
+    public function close() {
         $this->mysqli->close();
     }
 
-    public function __destruct()
-    {
+    public function __destruct() {
         $this->close(); 
     }
 }
-
 ?>
